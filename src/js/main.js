463d86b143cca06f7e885f4a92d859a24673ca85
OscarDu93 <oscar.tomasetti@studentfr.ch>
2025-11-11 10:56:30 +0100

    intro gsap

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,22 @@
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.to("#object-star-1", {
+  opacity: 0,
+  y: "-20%",
+  scrollTrigger: {
+    trigger: "#object-star-1",
+    start: "top top",
+    scrub: 1,
+  },
+});
+
+gsap.to("#object-star-2", {
+  opacity: 0,
+  y: "-50%",
+  scrollTrigger: {
+    trigger: "#object-star-2",
+    start: "bottom bottom",
+    scrub: 1,
+  },
+});
