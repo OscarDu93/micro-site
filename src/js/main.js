cf901d96ce66d8ca858695abd27c4c4a24ff856f
OscarDu93 <oscar.tomasetti@studentfr.ch>
2025-11-11 11:32:48 +0100

    slider start

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,11 @@
+// Les imports
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-gsap.to("#object-star-1", {
-  opacity: 0,
-  y: "-20%",
-  scrollTrigger: {
-    trigger: "#object-star-1",
-    start: "top top",
-    scrub: 1,
-  },
-});
+// Puis les déclarations de plugins (registerPlugin)
+gsap.registerPlugin(ScrollTrigger);
 
-gsap.to("#object-star-2", {
-  opacity: 0,
-  y: "-50%",
-  scrollTrigger: {
-    trigger: "#object-star-2",
-    start: "bottom bottom",
-    scrub: 1,
-  },
+document.addEventListener("DOMContentLoaded", function () {
+  //code JS ici
+  //parallax intro
 });
