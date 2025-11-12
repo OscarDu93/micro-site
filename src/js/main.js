0b68918b4c951ee1b5c25f44984232d9a209c22c
OscarDu93 <oscar.tomasetti@studentfr.ch>
2025-11-12 16:51:25 +0100

    mise en page

--- src/js/main.js
+++ src/js/main.js
@@ -38,4 +38,26 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
   // scroll parallax
+
+  gsap.to("#txt-left-1", {
+    opacity: 1,
+    x: "100%",
+    scrollTrigger: {
+      trigger: "#txt-left-1",
+      start: "bottom bottom",
+      end: "+=10%",
+      scrub: 1,
+    },
+  });
+
+  gsap.to("#img-right-2", {
+    opacity: 1,
+    x: "-140%",
+    scrollTrigger: {
+      trigger: "#txt-left-1",
+      start: "bottom bottom",
+      end: "+=20%",
+      scrub: 2,
+    },
+  });
 });
