b7cd04f105d92e850ce7e05de3d40da979ac2279
OscarDu93 <oscar.tomasetti@studentfr.ch>
2025-11-12 11:58:20 +0100

    debut js

--- src/js/main.js
+++ src/js/main.js
@@ -8,4 +8,34 @@ gsap.registerPlugin(ScrollTrigger);
 document.addEventListener("DOMContentLoaded", function () {
   //code JS ici
   //parallax intro
+
+  // scroll image
+  gsap.to(".slider-track", {
+    scrollTrigger: {
+      trigger: ".slider-mask",
+      start: "top top",
+      end: "+=400%",
+      pin: true,
+      scrub: true,
+    },
+    x: "-50%",
+    ease: "sine.inOut",
+  });
+
+  // zoom
+  gsap
+    .timeline({
+      scrollTrigger: {
+        trigger: ".presentation",
+        start: "top top",
+        scrub: 2,
+        pin: true,
+      },
+    })
+    .to("#zoom-element", {
+      width: 500,
+      height: 500,
+    });
+
+  // scroll parallax
 });
