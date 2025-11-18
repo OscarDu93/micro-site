565f6d34bcac8018a1db6d6895951a16ecf2d05e
OscarDu93 <oscar.tomasetti@studentfr.ch>
2025-11-18 16:46:42 +0100

    gsap start

--- src/js/main.js
+++ src/js/main.js
@@ -28,16 +28,39 @@ document.addEventListener("DOMContentLoaded", function () {
       scrollTrigger: {
         trigger: ".presentation",
         start: "top top",
-        scrub: 2,
+        scrub: 1,
+        ease: "slow(0.7,0.7,false)",
         pin: true,
       },
     })
-    .to("#zoom-element", {
-      width: 500,
-      height: 500,
+    .to(".zoom-container", {
+      scale: 1.2,
+      ease: "none",
+      opacity: 1,
     });
 
-  // scroll parallax
+  gsap.to(".zoom-container", {
+    y: "100%",
+    scrollTrigger: {
+      trigger: ".presentation",
+      start: "+=5%",
+      end: "bottom bottom",
+      scrub: 3,
+    },
+  });
+
+  gsap.to(".zoom-container", {
+    ease: "slow(0.7,0.7,false)",
+    opacity: 0,
+    scrollTrigger: {
+      trigger: ".presentation",
+      start: "bottom bottom",
+      end: "+=10%",
+      scrub: 3,
+    },
+  });
+
+  // scroll parallax 2
 
   gsap.to("#txt-left-1", {
     opacity: 1,
@@ -50,14 +73,47 @@ document.addEventListener("DOMContentLoaded", function () {
     },
   });
 
+  gsap
+    .timeline({
+      scrollTrigger: {
+        trigger: "#txt-left-1",
+        start: "top top",
+        scrub: 2,
+        ease: "slow(0.7,0.7,false)",
+      },
+    })
+    .to("#txt-left-1", {
+      y: "-20%",
+      ease: "none",
+      opacity: 0,
+    });
+
   gsap.to("#img-right-2", {
     opacity: 1,
     x: "-140%",
     scrollTrigger: {
       trigger: "#txt-left-1",
       start: "bottom bottom",
-      end: "+=20%",
+      end: "+=50%",
       scrub: 2,
     },
   });
+
+  gsap
+    .timeline({
+      scrollTrigger: {
+        trigger: "#txt-left-1",
+        start: "top top",
+        scrub: 5,
+        ease: "slow(0.7,0.7,false)",
+        pin: true,
+      },
+    })
+    .to("#img-right-2", {
+      y: "-40%",
+      ease: "none",
+      opacity: 0,
+    });
+
+  // scroll parallax 3
 });
